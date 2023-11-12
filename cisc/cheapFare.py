ec52f4b1b76b5a9806680152c85ce7b4c04143c3
Almas <apervmm@gmail.com>
2023-11-11 17:06:39 -0800

    cisc

---
+++ cisc/cheapFare.py
@@ -0,0 +1,8 @@
+from typing import List
+
+def cheapfare(n: int, m: int, trips: List[List[int]]): 
+    
+
+
+n, m = 4, 6
+trips = [[0,1, 20], [1, 0, 20], [1, 2, 10], [2, 0, 10], [1,3, 30], [3, 2, 30]]
---
+++ cisc/reversewords.py
@@ -0,0 +1,46 @@
+"""
+Given a String S, reverse the string without reversing its individual words. Words are separated by dots.
+
+Example 1:
+
+Input:  S = i.like.this.program.very.much
+Output:     much.very.program.this.like.i
+Explanation: After reversing the whole
+            string(not individual words), the input
+            string becomes
+            much.very.program.this.like.i
+"""
+
+
+def reverseWords(S: str):
+    # Vanila Code
+    newstr = S.split(".")
+    newstr.reverse()
+    return ".".join(newstr)
+
+
+def reverseWordsAlgo(S: str):
+    # Some hard code
+    ar = []
+    start, end = 0, 0
+    for i in range(len(S)):
+        c = S[i]
+        if c == ".":
+            end = i
+            ar.append(S[start: end])
+            start = end + 1
+        
+        if i == len(S) - 1:
+            ar.append(S[start: i+1])
+
+    L, R = 0, len(ar) -1
+    while L < R:
+        temp = ar[L]
+        ar[L] = ar[R]
+        ar[R] = temp
+        L += 1
+        R -= 1
+
+    return ".".join(ar)
+
+print(reverseWordsAlgo("much.very.program.this.like.i"))
